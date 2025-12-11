e5c41c2d5d76738030a58baea680d62718880872
elder lira <elderfranca@hotmail.com>
2025-12-11 16:27:05 -0300

    started processo to modularize code

---
+++ lib/history/base/index.dart
@@ -0,0 +1,11 @@
+class StoryPage {
+  final int pageNumber;
+  final String text;
+  final String imageDescription;
+
+  StoryPage({
+    required this.pageNumber,
+    required this.text,
+    required this.imageDescription,
+  });
+}
